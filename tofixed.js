60407b21da99783ea45806bb76651782f760ce2b
mounika <mounikamusham12@gmail.com>
2021-12-13 18:08:52 +0530

    tofixed

---
+++ tofixed.js
@@ -0,0 +1,9 @@
+var x=25.098536;
+console.log(typeof(x));
+console.log(x.toFixed(3));
+console.log(typeof(x.toFixed));
+
+var x=25.098536;
+console.log(typeof(x));
+console.log(x.toFixed(15));
+console.log(typeof(x.toFixed));
